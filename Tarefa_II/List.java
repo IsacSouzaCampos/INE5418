3561fbc57047b85542cc2a715a01726cdcb3f5c1
isacsouzacampos <isaccampos@hotmail.com>
2022-10-28 11:31:52 -0300

    Adicionado controle de concorrência

--- Tarefa_II/List.java
+++ Tarefa_II/List.java
@@ -1,9 +1,12 @@
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject; 
 import java.util.HashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class List extends UnicastRemoteObject implements ListInterface{
 	HashMap<Integer,String> list;
+	private static Lock lock = new ReentrantLock();
 	
 	public List() throws RemoteException {
 		list = new HashMap<Integer,String>();
@@ -11,18 +14,25 @@ public class List extends UnicastRemoteObject implements ListInterface{
 
 	@Override
 	public void add(int key, String value) throws RemoteException {
-		list.put(key, value);		
+		lock.lock();
+		list.put(key, value);
+		lock.unlock();
 	}
 	
 	@Override
 	public String get(int key) throws RemoteException {
-		return list.get(key);
+		lock.lock();
+		String value = list.get(key);
+		lock.unlock();
+		
+		return value;
 	}
 
 	@Override
 	public String remove(int key) throws RemoteException {
 		String value = this.get(key);
 		list.remove(key);
+		
 		return value;
 	}
 }
